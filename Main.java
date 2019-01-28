cc5fa4de9b8a944bf8cbf50e55e9c3647c20355f
Dongu <gazalpreetguddu@trentu.ca>
2019-01-27 19:36:37 -0500

    committing the program project file

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 /*  Gazalpreet Guddu
     This class asks for user input from main() and calculates the fibonacci sequence
     Both recursively using fiboSeriesRec() and iteratively using fiboSeriesIte()
-    Assumption: User will only input numbers and not other characters
-    */
+    Assumption: User will only input numbers */
 
 //Explicitly import the Scanner and TimeUnit for reading user input and elapsed time, respectively
 import java.util.Scanner;
@@ -24,6 +23,7 @@ public class Main {
             fiboSeriesRec(n, 1, 0, 0);
             long endtime = System.nanoTime();
 
+            //Calculate and output the elapsed time for the recursive method
             long timeElapsed = endtime - starttime;
             System.out.println("\nRecursive Method Execution Time: " + timeElapsed + " ns");
 
@@ -33,9 +33,11 @@ public class Main {
             fiboSeriesIte(n);
             endtime = System.nanoTime();
 
+            //Calculate and output the elapsed time for the iterative method
             timeElapsed = endtime - starttime;
             System.out.println("\nIterative Method Execution Time: " + timeElapsed + " ns");
         }
+        //Else statement in case user input is negative
         else
             System.out.println("Number cannot be less than 0");
     }
@@ -45,13 +47,15 @@ public class Main {
     //Prints the fibonacci sequence till the n value using recursion
     //Repeatedly calls itself while first is less than n, and prints the calculated value of first
     private static void fiboSeriesRec(float n, int third, int second, int first) {
+        //Checks if current total stored in first is less than user input and adds the last two values together
         if (first<n) {
+            //Last two values of the fibonacci sequence is stored in second and third
             first += second;
             second = third;
             third = first;
-            if (first<n)
+            if (first<n)    //Checks and prints the value if it is less than the user input
                 System.out.print(first+" ");
-            fiboSeriesRec(n, third, second, first);
+            fiboSeriesRec(n, third, second, first);     //Returns the user inputs, the last two values and the new total
         }
     }
 
@@ -61,13 +65,15 @@ public class Main {
     //For loop runs while first is less than n, and prints the calculated value of first
     private static void fiboSeriesIte(float n)
     {
+        //second and third will store the last two fibonacci values
         int second;
         int third = 1;
+        //Runs the loop while first is less than the user input; first keeps the calculated total value
         for(int first = 0; first<n; first += second)
         {
             second= third;
             third = first;
-            if (first<n)
+            if (first<n)    //Checks and prints the value if it is less than the user input
                 System.out.print(first+" ");
         }
     }
